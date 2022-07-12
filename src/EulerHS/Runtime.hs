97bc9358858d47266b5a649fc2cc0107dbcf3529
Hemant Mangla <hemant.mangla@juspay.in>
2022-07-12 17:21:08 +0530

    Resolved comments

--- src/EulerHS/Runtime.hs
+++ src/EulerHS/Runtime.hs
@@ -1,3 +1,30 @@
+{- |
+Module      :  EulerHS.Runtime
+Copyright   :  (C) Juspay Technologies Pvt Ltd 2019-2022
+License     :  Apache 2.0 (see the file LICENSE)
+Maintainer  :  opensource@juspay.in
+Stability   :  experimental
+Portability :  non-portable
+This is a top module that reexports all the runtime-specific types and functions.
+This layer of the framework contains methods for creating and disposing runtimes
+of different subsystems: logger, SQL, state and others.
+You typically create a single `FlowRuntime` instance and then use it to run your
+`Flow` scenarios.
+This module is better imported as qualified.
+@
+import qualified EulerHS.Types as T
+import qualified EulerHS.Language as L
+import qualified EulerHS.Runtime as R
+import qualified EulerHS.Interpreters as R
+myFlow :: L.Flow ()
+myFlow = L.runIO $ putStrLn @String "Hello there!"
+runApp :: IO ()
+runApp = do
+  let mkLoggerRt = R.createLoggerRuntime T.defaultFlowFormatter T.defaultLoggerConfig
+  R.withFlowRuntime (Just mkLoggerRt) $ \flowRt -> R.runFlow flowRt myFlow
+@
+-}
+
 module EulerHS.Runtime
   ( module X
   ) where
--- src/EulerHS/Types.hs
+++ src/EulerHS/Types.hs
@@ -1,3 +1,31 @@
+{- |
+Module      :  EulerHS.Types
+Copyright   :  (C) Juspay Technologies Pvt Ltd 2019-2022
+License     :  Apache 2.0 (see the file LICENSE)
+Maintainer  :  opensource@juspay.in
+Stability   :  experimental
+Portability :  non-portable
+This is a top module that reexports all the public types of the framework
+along with some helper functions.
+This module is better imported as qualified.
+@
+import qualified EulerHS.Types as T
+-- Beam imports
+import Database.Beam.MySQL (MySQLM)
+mySQLDevConfig :: T.'DBConfig' MySQLM
+mySQLDevConfig = T.'mkMySQLPoolConfig' "MySQL dev DB" cfg poolCfg
+  where
+    cfg :: T.'MySQLConfig'
+    cfg = T.'defaultMySQLConfig'
+      { T.connectPassword = "my pass"
+      , T.connectDatabase = "my db"
+      }
+    poolCfg = T.'defaultPoolConfig'
+      { T.keepAlive = 1000000
+      }
+@
+-}
+
 module EulerHS.Types
   ( module X
   ) where
