1ad52a05a7d949090f034495a721f5564c390865
Arjun Kathuria <arjun.kathuria.ext@juspay.in>
2022-12-26 13:58:56 +0530

    Update to Aeson 2 syntax
    * Mirgrate from old Aeson < 1.5 syntax to new Aeson > 2.0.1 syntax
    * Marshall types and functions as required

--- src/EulerHS/Masking.hs
+++ src/EulerHS/Masking.hs
@@ -9,6 +9,9 @@ import qualified Data.Aeson as Aeson
 import qualified Data.CaseInsensitive as CI
 import qualified Data.HashMap.Strict as HashMap
 import qualified Data.HashSet as HS
+import qualified Data.Aeson.KeyMap as KeyMap
+import qualified Data.Aeson.Key as AesonKey
+import qualified Data.Bifunctor as Bifunctor
 import qualified Data.List as List
 import qualified Data.Map as Map
 import qualified Data.Text as Text
@@ -80,7 +83,7 @@ handleObject shouldMask maskText mbContentType = HashMap.mapWithKey maskingFn
     maskingFn key value = bool (maskJSON shouldMask maskText mbContentType value) (Aeson.String maskText) $ shouldMask key
 
 handleQueryString :: ByteString -> Aeson.Value
-handleQueryString strg = Aeson.Object . fmap (Aeson.String . fromMaybe "") . HashMap.fromList $ HTTP.parseQueryText strg
+handleQueryString strg = Aeson.Object . fmap (Aeson.String . fromMaybe "") . KeyMap.fromList . map (Bifunctor.first AesonKey.fromText) $ HTTP.parseQueryText strg
 
 notSupportedPlaceHolder :: Maybe ByteString -> Aeson.Value
 notSupportedPlaceHolder (Just bs) = Aeson.String $ "Logging Not Support For this content " <> decodeUtf8 bs
