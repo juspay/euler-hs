1e7f7a4f3f768ee5eff514c2f96377e541a7d9e0
Sridhar Ratnakumar <srid@srid.ca>
2023-03-03 12:25:56 -0500

    Fix another warning

--- src/EulerHS/Framework/Flow/Interpreter.hs
+++ src/EulerHS/Framework/Flow/Interpreter.hs
@@ -276,10 +276,10 @@ interpretFlowMethod _ R.FlowRuntime {..} (L.DelOption k next) =
     let newMap = Map.delete k m
     putMVar _options newMap
 
-interpretFlowMethod _ R.FlowRuntime {..} (L.GenerateGUID next) = do
+interpretFlowMethod _ _ (L.GenerateGUID next) = do
   next <$> (UUID.toText <$> UUID.nextRandom)
 
-interpretFlowMethod _ R.FlowRuntime {..} (L.RunSysCmd cmd next) =
+interpretFlowMethod _ _ (L.RunSysCmd cmd next) =
   next <$> readCreateProcess (shell cmd) ""
 
 ----------------------------------------------------------------------
@@ -290,7 +290,7 @@ interpretFlowMethod mbFlowGuid rt (L.Fork _desc _newFlowGUID flow next) = do
 
 ----------------------------------------------------------------------
 
-interpretFlowMethod _ R.FlowRuntime {..} (L.Await mbMcs (T.Awaitable awaitableMVar) next) = do
+interpretFlowMethod _ _ (L.Await mbMcs (T.Awaitable awaitableMVar) next) = do
   let act = case mbMcs of
         Nothing -> do
           val <- readMVar awaitableMVar
@@ -300,7 +300,7 @@ interpretFlowMethod _ R.FlowRuntime {..} (L.Await mbMcs (T.Awaitable awaitableMV
         Just (T.Microseconds mcs) -> awaitMVarWithTimeout awaitableMVar $ fromIntegral mcs
   next <$> act
 
-interpretFlowMethod _ R.FlowRuntime {..} (L.ThrowException ex _) = do
+interpretFlowMethod _ _ (L.ThrowException ex _) = do
   throwIO ex
 
 interpretFlowMethod mbFlowGuid rt (L.CatchException comp handler cont) =
