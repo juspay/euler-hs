349b39167de4ad5fb17adec381b5dc9749ca7869
Arjun Kathuria <arjun.kathuria8@gmail.com>
2023-10-10 11:49:34 +0530

    Update flake files.
    * Updates flake.nix and flake.lock files
    
    * flake.nix changes:
      1. updates `common` repo, now supports GHC 9.2.7 package-set.
      2. updates `cereal` repo, updated to now support GHC-9.2.7.
      3. updates `sequelize` repo, is now a flake-module and updated
         to now support GHC-9.2.7
      4. updates `servant-mock`, to one that builds with GHC-9.2.7.
      5. updates `tinylog`, to one that builds with GHC-9.2.7
      6. removes now uneeded inputs like:
         - beam
         - beam-mysql
         - mysql-haskell
    
      7. imports ghc927 flake-module and package-set
      8. imports new `sequelize` flake-module
      9. basePackages is now the ghc927 package-set
      10. some required package overrides
      11. autoWire "packages", "checks", "devShells" and "apps"
    
    * flake.lock file updated with all the changes above

--- flake.nix
+++ flake.nix
@@ -1,40 +1,43 @@
 {
   inputs = {
     # Common is used only to get the GHC 8.10 package set.
-    common.url = "github:nammayatri/common";
+    # common.url = "github:nammayatri/common";
+
+    # Replace the url with upstream after the GHC 9.2 changes get merged there.
+    common.url = "github:arjunkathuria/common/Mobility-GHC927-rebased-04";
     nixpkgs.follows = "common/nixpkgs";
     flake-parts.follows = "common/flake-parts";
     haskell-flake.follows = "common/haskell-flake";
 
-    # Haskell dependencies
-    cereal.url = "github:juspay/cereal/213f145ccbd99e630ee832d2f5b22894c810d3cc";
+    cereal.url = "github:juspay/cereal";
     cereal.flake = false;
 
     juspay-extra.url = "github:juspay/euler-haskell-common";
-    juspay-extra.inputs.haskell-flake.follows = "common/haskell-flake";
+    juspay-extra.inputs.haskell-flake.follows = "haskell-flake";
 
     euler-events-hs.url = "github:juspay/euler-events-hs/main";
-    euler-events-hs.inputs.haskell-flake.follows = "common/haskell-flake";
+    euler-events-hs.inputs.haskell-flake.follows = "haskell-flake";
 
-    sequelize.url = "github:juspay/haskell-sequelize/dc01b0f9e6ba5a51dd8f9d0744a549dc9c0ba244";
-    sequelize.flake = false;
+    sequelize.url = "github:juspay/haskell-sequelize/beckn-compatible";
+    sequelize.inputs.nixpkgs.follows = "nixpkgs";
+    sequelize.inputs.haskell-flake.follows = "haskell-flake";
+    sequelize.inputs.flake-parts.follows = "flake-parts";
 
-    beam.url = "github:srid/beam/ghc810"; 
-    beam.flake = false;
-
-    beam-mysql.url = "github:juspay/beam-mysql/b4dbc91276f6a8b5356633492f89bdac34ccd9a1";
-    beam-mysql.flake = false;
-
-    mysql-haskell.url = "github:juspay/mysql-haskell/788022d65538db422b02ecc0be138b862d2e5cee"; # https://github.com/winterland1989/mysql-haskell/pull/38
-    mysql-haskell.flake = false;
     hedis.url = "git+https://github.com/juspay/hedis?rev=92a3d5ab73dcb0ea11139a01d6f2950a8b8e7e0e";
     hedis.flake = false;
+
+    servant-mock.url = "github:arjunkathuria/servant-mock?rev=17e90cb831820a30b3215d4f164cf8268607891e";
+    servant-mock.flake = false;
+
+    tinylog.url = "git+https://gitlab.com/arjunkathuria/tinylog.git?rev=08d3b6066cd2f883e183b7cd01809d1711092d33";
+    tinylog.flake = false;
   };
+
   outputs = inputs@{ nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [
-        inputs.common.flakeModules.ghc810
+        inputs.common.flakeModules.ghc927
         inputs.haskell-flake.flakeModule
       ];
       perSystem = { self', pkgs, lib, config, ... }: {
@@ -44,55 +47,19 @@
           imports = [
             inputs.euler-events-hs.haskellFlakeProjectModules.output
             inputs.juspay-extra.haskellFlakeProjectModules.output
+            inputs.sequelize.haskellFlakeProjectModules.output
           ];
-          basePackages = config.haskellProjects.ghc810.outputs.finalPackages;
+          basePackages = config.haskellProjects.ghc927.outputs.finalPackages;
           packages = {
-            beam-core.source = inputs.beam + /beam-core;
-            beam-migrate.source = inputs.beam + /beam-migrate;
-            beam-mysql.source = inputs.beam-mysql;
-            beam-postgres.source = inputs.beam + /beam-postgres;
-            beam-sqlite.source = inputs.beam + /beam-sqlite;
             hedis.source = inputs.hedis;
-            mysql-haskell.source = inputs.mysql-haskell;
-            sequelize.source = inputs.sequelize;
             cereal.source = inputs.cereal;
+            servant-mock.source = inputs.servant-mock;
+            tinylog.source = inputs.tinylog;
           };
           settings = {
-            beam-core.jailbreak = true;
-            beam-migrate.jailbreak = true;
-            beam-mysql = {
-              check = false;
-              jailbreak = true;
-            };
-            beam-postgres = {
-              check = false;
-              jailbreak = true;
-            };
-            beam-sqlite = {
-              check = false;
-              jailbreak = true;
-            };
+            bytestring-conversion.broken = false;
             hedis.check = false;
-            mysql-haskell = {
-              check = false;
-              jailbreak = true;
-            };
             sequelize.check = false;
-            cereal = {
-              check = false;
-              jailbreak = true;
-            };
-            euler-events-hs = {
-              check = false;
-              jailbreak = true;
-            };
-            juspay-extra = {
-              check = false;
-              jailbreak = true;
-            };
-            nonempty-containers = {
-              jailbreak = true;
-            };
             servant-client = {
               jailbreak = true;
             };
@@ -102,7 +69,15 @@
             servant-server = {
               jailbreak = true;
             };
+            servant-mock = {
+              check = false;
+              jailbreak = true;
+            };
+            servant = {
+              jailbreak = true;
+            };
           };
+          autoWire = [ "packages" "checks" "devShells" "apps"];
         };
       };
     };
